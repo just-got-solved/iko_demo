76e67d151033b2eb99a9054890bedd6dd4a5fcb4
johnatan.souza <jgs8115@gmail.com>
2023-11-04 16:09:17 -0300

    add produtos app

--- demo/contatos/urls.py
+++ demo/contatos/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('incluir/', views.cadastrar_informacao_contato, name='add_contatos'),
     path('cadastrar_contato/', views.cadastrar_informacao_contato, name='cadastrar_contato'),
-    # path('lista/', views.lista_produtos, name='lista_produtos'),
-    # path('pesquisa/', views.pesquisa, name='pesquisa'),
 ]
