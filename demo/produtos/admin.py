a864b423d5d72eb721ee904bc55992dc9711bdeb
JGS <136353034+just-got-solved@users.noreply.github.com>
2023-11-04 16:48:19 -0300

    Update admin.py

--- demo/produtos/admin.py
+++ demo/produtos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Produto, ProdutoConcorrenteFag, ProdutoConcorrenteIna, ProdutoConcorrenteSkf, ProdutoConcorrenteTorrington
+from .models import Produto
 # Register your models here.
 
 admin.site.register(Produto)
