721295e5d78faccb50ed2a08482ed46eb5101b79
johnatan.souza <jgs8115@gmail.com>
2023-11-04 19:04:34 -0300

    edit urls contatos

--- demo/contatos/urls.py
+++ demo/contatos/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('incluir/', views.cadastrar_informacao_contato, name='add_contatos'),
-    path('cadastrar_contato/', views.cadastrar_informacao_contato, name='cadastrar_contato'),
 ]
