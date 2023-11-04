5196f07fe97dd9fc69933a08c2b90229c960ea84
johnatan.souza <jgs8115@gmail.com>
2023-11-04 20:57:18 -0300

    edit admin produtos

--- demo/produtos/admin.py
+++ demo/produtos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, ProdutoConcorrenteFag, ProdutoConcorrenteIna, ProdutoConcorrenteSkf, ProdutoConcorrenteTorrington
 # Register your models here.
 
-admin.site.register(Produto)
+admin.site.register([Produto,ProdutoConcorrenteFag,ProdutoConcorrenteIna,ProdutoConcorrenteSkf,ProdutoConcorrenteTorrington])
